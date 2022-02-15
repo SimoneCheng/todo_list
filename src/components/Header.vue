88651f75561efede8e1d726292ea031d9b104f35
simone <simone@fugle.tw>
2022-02-15 15:57:07 +0800

    init repo

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="header">
+    <h1 class="container"><strong>Todo List</strong></h1>
+    <div class="container">為你的金魚腦增加容量吧！</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+ #header {
+     display: flex;
+     background-color: black;
+     color: white;
+     justify-content: center;
+     align-items: center;
+ }
+
+ .container {
+     margin: 10px;
+ }
+</style>
